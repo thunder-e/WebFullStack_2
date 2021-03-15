854f1b03c6c69467061c52b1f385278e77e37778
thunder-e <79903353+thunder-e@users.noreply.github.com>
2021-03-15 15:04:01 +0900

    dao

--- src/com/sunwoo/s1/member/MemberDAO.java
+++ src/com/sunwoo/s1/member/MemberDAO.java
@@ -1,5 +1,61 @@
 package com.sunwoo.s1.member;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class MemberDAO {
+	
+	//login - id pw를 받아서 조회
+	public MemberDTO login(MemberDTO memberDTO) throws Exception {
+		 
+		//1. 로그인 정보
+		String user = "user01";
+		String password = "user01";
+		String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		
+		//2. 클래스 로딩 - 드라이버명 명시
+		Class.forName(driver);
+		
+		//3. 로그인 Connection
+		Connection con = DriverManager.getConnection(url, user, password);
+		
+		//4. SQL문 생성
+		String sql = "select * from member where id=? and pw=?";
+		
+		//5. 미리 보내기
+		PreparedStatement st = con.prepareStatement(sql);
+		
+		//6. ? 세팅
+		st.setString(1, memberDTO.getId());
+		st.setString(2, memberDTO.getPw());
+		
+		//7. 최종 전송 후 처리
+		ResultSet rs = st.executeQuery();
+		
+		
+		
+		if(rs.next()) {
+			memberDTO.setName(rs.getString("name"));
+			memberDTO.setPhone(rs.getString("phone"));
+			memberDTO.setEmail(rs.getString("email"));
+			
+		} else {
+			memberDTO = null;
+		}
+		
+		//8. 연결 해제
+		rs.close();
+		st.close();
+		con.close();
+		
+		
+		return memberDTO;
+		
+	}
+	
+	
 
 }
---
+++ src/com/sunwoo/s1/test/MemberDAOTest.java
@@ -0,0 +1,28 @@
+package com.sunwoo.s1.test;
+
+import com.sunwoo.s1.member.MemberDAO;
+import com.sunwoo.s1.member.MemberDTO;
+
+public class MemberDAOTest {
+
+	public static void main(String[] args) {
+		//Web에서 메인 메서드는 테스트 용도로만 사용
+		MemberDAO memberDAO = new MemberDAO();
+		MemberDTO memberDTO = new MemberDTO();
+		memberDTO.setId("iu");
+		memberDTO.setPw("pw1");
+		
+		try {
+			memberDTO = memberDAO.login(memberDTO);
+			
+			System.out.println(memberDTO != null);
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
